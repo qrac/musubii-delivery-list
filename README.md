491167cc61055335f858f4b4276c686de89f7be5
Qrac <qrac.jp@gmail.com>
2018-05-22 13:59:30 +0900

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# musubii-delivery-list
+# MUSUBii Delivery List
+
+## Demo & Documentation
+
+* [Demo Page][link-demo]
+
+## About
+
+* クライアントに納品するときの HTML リストテンプレート
+* ただの目次だけど最低限の見栄えで納品したい
+* Pug を使いできるだけ簡単に の 1 ファイル完結で生成
+* プレビューに CDN の MUSUBii と YakuHanJP を使用
+* 納品ページと GitHub リポジトリへのリンクを生成
+
+## Use
+
+* `src/pug/develop.pug` の Variable を編集して使用
+* favicon 類を設定するとなお良し
+
+## License
+
+* MIT
+* Author: [Qrac][link-twitter]
+* Author Group: [QRANOKO][link-qranoko]
+
+[link-demo]: https://qrac.github.io/musubii-delivery-list/develop.html
+[link-twitter]: https://twitter.com/Qrac_JP
+[link-qranoko]: https://qranoko.jp
---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+//----------------------------------------------------
+// Gulp > npx gulp
+//----------------------------------------------------
+
+const gulp = require('gulp');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const browserSync = require('browser-sync');
+const pug = require('gulp-pug');
+
+// Setting : Paths
+const paths = {
+  'src_pug': './src/pug/',
+  'out_html': './docs/'
+}
+
+// Setting : Pug Options
+const pugOptions = {
+  pretty: true
+}
+
+// Pug > HTML
+gulp.task('pug', () => {
+  return gulp.src([paths.src_pug + '**/*.pug', '!' + paths.src_pug + '**/_*.pug'])
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(pug(pugOptions))
+    .pipe(gulp.dest(paths.out_html));
+});
+
+// Browser Sync
+gulp.task('browser-sync', function (done) {
+  browserSync.init({
+    server: {
+      baseDir: paths.out_html
+    },
+    startPath: './develop.html',
+    notify: false
+  });
+  done();
+});
+
+gulp.task('reload', function (done) {
+  browserSync.reload();
+  done();
+});
+
+// Watch
+gulp.task('watch', () => {
+  gulp.watch([paths.src_pug + '**/*.pug', '!' + paths.src_pug + '**/_*.pug'], gulp.series('pug', 'reload'));
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
